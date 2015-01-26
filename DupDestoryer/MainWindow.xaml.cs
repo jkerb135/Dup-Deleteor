851fb59f82f5ef77f70216a1c1efa3b39c722578
Josh Kerbaugh <jkerb135@live.kutztown.edu>
2015-01-26 10:26:37 -0500

    Initial Commit

---
+++ DupDestoryer/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Mime;
+using MahApps.Metro.Controls;
+
+namespace DupDestoryer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            Title = "Dup Destroyer";
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            Box.Text = dialog.SelectedPath;
+        }
+    }
+}
